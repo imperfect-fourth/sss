394d1f4caab1554b92f2f2ca85c7ded0a36e0913
Divi <divyansh@hasura.io>
2021-12-23 08:19:40 +0530

    add start command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from discord import Embed, File
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
@@ -14,6 +15,13 @@ bot = commands.Bot(command_prefix='sss ')
 async def on_ready():
     print('Secret Santa Services reporting for duty')
 
+
+@bot.command(name='start')
+async def start(ctx):
+    embed = Embed(title='Secret Santa Services', description='hoe hoe hoe SSS reporting for duty\nReact with :santa: to take part in Secret Santa :D', color=0xef2929)
+    santa = File('santa.png')
+    embed.set_thumbnail(url='attachment://santa.png')
+    await ctx.send(file=santa, embed=embed)
 
 if __name__ == '__main__':
     bot.run(_BOT_TOKEN)
