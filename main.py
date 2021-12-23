7c92c3032ebd392c5c2faef98ee3a780becc37d8
Divi <divyansh@hasura.io>
2021-12-23 09:50:42 +0530

    add reaction role on message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from discord import Embed, File
+from discord import Embed, File, Intents, utils
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
@@ -8,8 +8,16 @@ import os
 load_dotenv()
 _BOT_TOKEN = os.getenv('SSS_BOT_TOKEN')
 
-bot = commands.Bot(command_prefix='sss ')
+intents = Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='sss ', intents=intents)
 
+global STATE, SANTA_IMG, SANTA_EMOJI
+SANTA_IMG = File('santa.png')
+SANTA_EMOJI = '🎅'
+STATE = {
+    'waiting_for_reacts': False,
+}
 
 @bot.event
 async def on_ready():
@@ -18,10 +26,74 @@ async def on_ready():
 
 @bot.command(name='start')
 async def start(ctx):
+    global STATE, SANTA_IMG, SANTA_EMOJI
+    santa_role = await ctx.guild.create_role(name='Secret Santa {}'.format(SANTA_EMOJI), colour=0xef2929, mentionable=True)
+    STATE['santa_role'] = santa_role
+    STATE['guild'] = ctx.guild
+
     embed = Embed(title='Secret Santa Services', description='hoe hoe hoe SSS reporting for duty\nReact with :santa: to take part in Secret Santa :D', color=0xef2929)
-    santa = File('santa.png')
     embed.set_thumbnail(url='attachment://santa.png')
-    await ctx.send(file=santa, embed=embed)
+
+    message = await ctx.send(file=SANTA_IMG, embed=embed)
+    await message.add_reaction(SANTA_EMOJI)
+    STATE['message'] = message
+    STATE['waiting_for_reacts'] = True
+    print('waiting for reactions')
+    
+
+def act_on_react(payload):
+    global STATE, SANTA_EMOJI
+    user_id = payload.user_id
+    user = STATE['guild'].get_member(user_id)
+    reaction = payload.emoji.name
+    message_id = payload.message_id
+    if user == bot.user:
+        return False
+
+    if not STATE['waiting_for_reacts']:
+        print('wait')
+        return False
+    if message_id != STATE['message'].id:
+        print('m')
+        return False
+    if reaction != SANTA_EMOJI:
+        print('e')
+        return False
+
+    return True
+
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    if not act_on_react(payload):
+        return
+    global STATE
+    user_id = payload.user_id
+    user = STATE['guild'].get_member(user_id)
+    if STATE['santa_role'] in user.roles:
+        return
+    await user.add_roles(STATE['santa_role'])
+    print('role added: {}'.format(user))
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    if not act_on_react(payload):
+        return
+    global STATE
+    user_id = payload.user_id
+    user = STATE['guild'].get_member(user_id)
+    if not STATE['santa_role'] in user.roles:
+        return
+    await user.remove_roles(STATE['santa_role'])
+    print('role removed: {}'.format(user))
+
+
+@bot.event
+async def on_disconnect():
+    global STATE
+    await STATE['santa_role'].delete()
+
 
 if __name__ == '__main__':
     bot.run(_BOT_TOKEN)
